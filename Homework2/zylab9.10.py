16c9af51cba4857fcf0ef3884c316695b89def6a
44dandurham <135792098+44dandurham@users.noreply.github.com>
2023-07-02 18:22:43 -0500

    Add files via upload

---
+++ Homework2/zylab9.10.py
@@ -0,0 +1,24 @@
+import csv
+
+input1 = input()
+
+uppercase_freq = {}
+lowercase_freq = {}
+
+with open(input1, 'r') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        for word in row:
+            word = word.strip()
+
+            if word.isupper():
+                uppercase_freq[word] = uppercase_freq.get(word, 0) + 1
+            else:
+                lowercase_freq[word] = lowercase_freq.get(word, 0) + 1
+
+
+for word, frequency in lowercase_freq.items():
+    print(f'{word} {frequency}')
+for word, frequency in uppercase_freq.items():
+    print(f'{word} {frequency}')
+
